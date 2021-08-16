5d751eb40dd2ed0da4e13a3dab13c79fb90cc91d
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-08-16 22:26:59 +0100

    Initialized project

---
+++ app/src/main/java/com/davidmarian_buzatu/datafy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.davidmarian_buzatu.datafy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
