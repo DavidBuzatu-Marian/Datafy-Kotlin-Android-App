7a7ec5b3a7a156b47a7f2caa5c1834f4951246be
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-08-18 19:42:34 +0100

    Added handlers for edit text

--- app/src/main/java/com/davidmarian_buzatu/datafy/BlogActivity.kt
+++ app/src/main/java/com/davidmarian_buzatu/datafy/BlogActivity.kt
@@ -1,21 +1,61 @@
 package com.davidmarian_buzatu.datafy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import com.davidmarian_buzatu.datafy.blog.BLOG
 import com.davidmarian_buzatu.datafy.models.Blog
+import com.google.android.material.textfield.TextInputEditText
 
 class BlogActivity : AppCompatActivity() {
+    private var activityBlog: Blog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blog)
 
-        val blog: Blog = intent.getSerializableExtra(BLOG) as Blog
+        setActivityBlog(intent)
+        setNonEditableInitialText()
+        setEditableInitialText()
+        setEditableListeners()
+    }
+
+    private fun setEditableListeners() {
+        val editTextBlogTitle: TextInputEditText = findViewById(R.id.activity_blog_tiet_title)
+        val editTextBlogContent: TextInputEditText = findViewById(R.id.activity_blog_tiet_content)
+
+        editTextBlogTitle.addTextChangedListener {
+            text -> activityBlog!!.setTitle(text.toString())
+            val textViewBlogTitle: TextView = findViewById(R.id.activity_blog_tv_title)
+            textViewBlogTitle.text = text
+         }
+
+        editTextBlogContent.addTextChangedListener {
+                text -> activityBlog!!.setContent(text.toString())
+            val textViewBlogContent: TextView = findViewById(R.id.activity_blog_tv_content)
+            textViewBlogContent.text = text
+        }
+    }
+
+    private fun setEditableInitialText() {
+        val editTextBlogTitle: TextInputEditText = findViewById(R.id.activity_blog_tiet_title)
+        val editTextBlogContent: TextInputEditText = findViewById(R.id.activity_blog_tiet_content)
+
+        editTextBlogTitle.setText(activityBlog!!.getTitle())
+        editTextBlogContent.setText(activityBlog!!.getContent())
+    }
+
+    private fun setNonEditableInitialText() {
         val textViewBlogTitle: TextView = findViewById(R.id.activity_blog_tv_title)
         val textViewBlogContent: TextView = findViewById(R.id.activity_blog_tv_content)
 
-        textViewBlogTitle.text = blog.getTitle()
-        textViewBlogContent.text = blog.getContent()
+        textViewBlogTitle.text = activityBlog!!.getTitle()
+        textViewBlogContent.text = activityBlog!!.getContent()
+    }
+
+    private fun setActivityBlog(intent: Intent) {
+        activityBlog = intent.getSerializableExtra(BLOG) as Blog
     }
 }
--- app/src/main/java/com/davidmarian_buzatu/datafy/models/Blog.kt
+++ app/src/main/java/com/davidmarian_buzatu/datafy/models/Blog.kt
@@ -2,9 +2,16 @@ package com.davidmarian_buzatu.datafy.models
 
 import java.io.Serializable
 
-class Blog(private val title: String, private val content: String, private val url: String) : Serializable {
+class Blog(private var title: String, private var content: String, private val url: String) : Serializable {
     fun getUrl() = url
     fun getContent() = content
     fun getTitle() = title
+    fun setTitle(title: String) {
+        this.title = title
+    }
+
+    fun setContent(content: String) {
+        this.content = content
+    }
 
 }
